c3b3714d56436e0d4dab6fc22b6f6b1e660697a7
jacquette <pierrick.jacquette@etu.univ-paris-diderot.fr>
2016-10-18 20:51:18 +0200

    mise a jour pour la synchronisation des roues

--- src/project/Carreau.java
+++ src/project/Carreau.java
@@ -4,25 +4,26 @@ import lejos.robotics.RegulatedMotor;
 import lejos.utility.Delay;
 
 public class Carreau {
-
 	public static void main(String[] args) {
-
-		EV3LargeRegulatedMotor b = new EV3LargeRegulatedMotor(MotorPort.B);
-		EV3LargeRegulatedMotor d = new EV3LargeRegulatedMotor(MotorPort.D);
+		RegulatedMotor b = new EV3LargeRegulatedMotor(MotorPort.B);
+		RegulatedMotor d = new EV3LargeRegulatedMotor(MotorPort.D);
 		b.synchronizeWith(new RegulatedMotor[] { d });
 
-		b.startSynchronization();
 		for (int i = 0; i < 4; i++) {
-			b.forward();
-			d.forward();
-			Delay.msDelay(3000);
-			b.rotate(90, true);
-			d.rotate(90, true);
-		}
-		b.endSynchronization();
+			avance(b, d);
 
-		b.waitComplete();
-		d.waitComplete();
+		}
+		b.stop();
+		d.stop();
 	}
 
+	public static void avance(RegulatedMotor b, RegulatedMotor d) {
+		b.startSynchronization();
+		b.forward();
+		d.forward();
+		b.endSynchronization();
+		Delay.msDelay(1500);
+		b.rotateTo(90);
+		
+	}
 }
