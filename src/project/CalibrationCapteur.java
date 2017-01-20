5636aad3a33079d31b3436ec3ffc4d3d7c07eb58
jacquette <pierrick.jacquette@etu.univ-paris-diderot.fr>
2017-01-20 13:59:04 +0100

    amelioration du temps de calibration

--- src/project/CalibrationCapteur.java
+++ src/project/CalibrationCapteur.java
@@ -12,20 +12,16 @@ public class CalibrationCapteur {
 
 	private PrintWriter p;
 	private Color couleur;
-	private String dernier;
 
 	public CalibrationCapteur() {
 		this.p = initFile();
 		this.couleur = new Color(0f, 0f, 0f, 0);
-		this.dernier = "";
 	}
 
 	protected void start() {
 		int name = 0;
-		int releve = 0, lastReleve = 0;
-		// vaut 1 si Button Up et vaut 2 si Button Right
-		int lastAction = 0;
-		boolean ok = false, lastOk = false;
+		int releve = 0;
+		boolean ok = false;
 		boolean loop = true;
 		while (loop) {
 			/*
@@ -39,7 +35,6 @@ public class CalibrationCapteur {
 			case Button.ID_UP:
 				for (int i = 0; i < 10; i++) {
 					this.write(name, releve);
-					lastReleve = releve;
 					releve++;
 					try {
 						Thread.sleep(50);
@@ -47,49 +42,17 @@ public class CalibrationCapteur {
 						e.printStackTrace();
 					}
 				}
-				lastOk = ok;
 				ok = true;
-				lastAction = 1;
+				LCD.drawString("Finie pour " + name, 0, 3);
+				Util.DelayClearLCD();
 				break;
 			case Button.ID_RIGHT:
 				if (ok) {
 					name++;
-					lastReleve = releve;
-					releve = 0;
-					for (int i = 0; i < 10; i++) {
-						this.write(name, releve);
-						try {
-							Thread.sleep(50);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-					}
-					releve++;
-
-					lastAction = 2;
 				} else {
 					LCD.drawString("Aucun releve ", 0, 3);
 					LCD.drawString("pour couleur " + name, 1, 4);
 					Util.DelayClearLCD();
-				}
-				break;
-			case Button.ID_LEFT:
-				LCD.drawString("Annulation du ", 0, 3);
-				LCD.drawString("Releve " + releve + " couleur " + name, 1, 4);
-				this.dernier = "";
-				switch (lastAction) {
-				case 1:
-					releve = lastReleve;
-					ok = lastOk;
-					lastAction = 0;
-					break;
-				case 2:
-					name--;
-					releve = lastReleve;
-					lastAction = 0;
-					break;
-				default:
-					break;
 				}
 				break;
 			case Button.ID_ESCAPE:
@@ -100,7 +63,6 @@ public class CalibrationCapteur {
 			}
 
 		}
-		this.p.println(this.dernier);
 		this.p.flush();
 		this.p.close();
 	}
@@ -108,11 +70,7 @@ public class CalibrationCapteur {
 	private void write(int name, int releve) {
 		this.couleur = Util.lireColor();
 		this.couleur.setName(name);
-		this.p.println(this.dernier);
-		this.dernier = this.couleur.toString();
-
-		LCD.drawString("Releve " + releve + " couleur " + name, 0, 3);
-		Util.DelayClearLCD();
+		this.p.println(this.couleur);
 	}
 
 	private PrintWriter initFile() {
