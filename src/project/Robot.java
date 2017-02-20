64e37f910db67b0b1006d2691535a71ddc329db3
ELBEZ samuel <elbez@informatique.univ-paris-diderot.fr>
2017-02-20 19:04:01 +0100

    correction erreur affichage

--- src/project/Robot.java
+++ src/project/Robot.java
@@ -74,9 +74,9 @@ public class Robot {
 		while (erreur == true) {
 			try {
 				this.colorRGBSensor.fetchSample(sample, 0);
-				LCD.drawString("Succes Fetch", 0, 1);
 				erreur = false;
 			} catch (I2CException e) {
+			    LCD.drawString("Une erreur est apparue lors du fetch", 0, 1);
 				erreur = true;
 			}
 		}
